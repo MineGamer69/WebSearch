b8449614dc6bafef55f86b88e90a88dd39a6cb92
NovaMatthew <mnova412@gamil.com>
2023-04-19 13:50:43 -0400

    ImageSearchViewModel.kt complete

---
+++ app/src/main/java/com/example/websearch/ImageSearchViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.websearch
+
+//Made By Aaryan Kapoor & Matt Nova
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class ImageSearchViewModel : ViewModel() {
+
+    val searchResults = MutableLiveData<List<ImageValue>>()
+
+    fun performSearch(query: String, safeSearchEnabled: Boolean) {
+        APIinterface.create(query).imageSearchResponse(query,1,10,true, safeSearchEnabled).enqueue(object : Callback<imageDataX> {
+            override fun onResponse(call: Call<imageDataX>, response: Response<imageDataX>) {
+                val results = response.body()?.value ?: emptyList()
+                searchResults.postValue(results)
+            }
+
+            override fun onFailure(call: Call<imageDataX>, t: Throwable) {
+                Log.d("crash", "API CALL FAILED!")
+            }
+        })
+    }
+
+}
